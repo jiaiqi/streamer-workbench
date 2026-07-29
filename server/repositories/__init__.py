d5b3318726edd79bc0a72296d83310db0a4a95c3
jiaqi <2468260248@qq.com>
2026-07-29 08:26:26 +0800

    feat(events): 实现带恢复索引的文件事件存储

--- server/repositories/__init__.py
+++ server/repositories/__init__.py
@@ -1,12 +1,15 @@
 """本地文件 Repository adapters。"""
 
 from .atomic_json import AtomicJsonWriter, FaultInjector, MISSING_REVISION, json_revision
+from .events import EventFaultInjector, FileEventStore
 from .settings import FileSettingsRepository
 from .songs import FileSongRepository
 
 __all__ = [
     "AtomicJsonWriter",
     "FaultInjector",
+    "EventFaultInjector",
+    "FileEventStore",
     "FileSettingsRepository",
     "FileSongRepository",
     "MISSING_REVISION",
